a49a3a63055c24544115f0bf9ae9b4ffef29a771
Leonardo Augusto <leonardoaugusto1000a@gmail.com>
2026-06-15 18:29:24 -0300

    feat: Updates the user table to ensure that email addresses are unique and adjusts the database initialization script

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -5,11 +5,12 @@ set -e
 ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
 docker build \
+  --no-cache \
   -f "$ROOT_DIR/Dockerfile/dockerfile-mysql" \
   -t meu-mysql \
   "$ROOT_DIR"
 
-  docker run -d \
+docker run -d \
   --name mysql-container \
   -p 3306:3306 \
   -v mysql_data:/var/lib/mysql \
